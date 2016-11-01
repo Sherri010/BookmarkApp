6fd4da34b2800a7db9d676fb1a9fd77ec136fd93
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2016-11-01 09:33:25 -0700

    Update bookmark.rb

--- bookmark.rb
+++ bookmark.rb
@@ -1,6 +1,7 @@
 require "data_mapper"
 # #creating a resource
 class Bookmark
+  #model is going to need to be persistent, so we'll include DataMapper::Resource.
   include DataMapper::Resource
    property :id,Serial
    property :url,String
