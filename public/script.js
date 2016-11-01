fa07876f4eda96a0f90fd1c8b1644e53442443cb
sherri010 <aminshahidi.sh@gmail.com>
2016-11-01 09:26:58 -0700

    removed create ajax call - not needed

--- public/script.js
+++ public/script.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
   console.log('js loaded');
 
   $('.remove').on('click',function(e){
-      var mark = $(this).attr("id")
-       console.log(mark);
-      var path = "/bookmarks/"+mark;
+      var path = "/bookmarks/"+ $(this).attr("id");
        $.ajax({
         type:'DELETE',
         url: path,
@@ -16,21 +14,4 @@ $(document).ready(function(){
        window.location.replace("/bookmarks");
 
    });
-
-   $('#add_form').on('submit',function(e){
-      e.preventDefault();
-      var url_data = $('#url').val();
-      var title_data = $('#title').val();
-
-      $.ajax({
-        type:'POST',
-        url:'/bookmarks',
-        data: {url:url_data,title:title_data},
-        success:function(data){
-          console.log('added')
-          alert(title_data+" deleted!");
-        }
-      });
-
-  });
 });
